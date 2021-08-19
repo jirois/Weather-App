961dbad3eb57b562ea061487e490a8fdfa96897c
Lin <omasajiri@gmail.com>
2021-08-19 09:11:22 +0100

    feat: setup app env

--- app/src/main/java/com/jinncyapps/weatherapp/MainActivity.kt
+++ app/src/main/java/com/jinncyapps/weatherapp/MainActivity.kt
@@ -1,11 +1,44 @@
 package com.jinncyapps.weatherapp
 
+import android.content.Context
+import android.content.Intent
+import android.location.LocationManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (!isLocationEnabled()){
+            Toast.makeText(
+                this,
+                "Your location provider is turned off. Please turn it on",
+                Toast.LENGTH_SHORT
+            ).show()
+
+            // This code will redirect you to settings from where you can turn on the location
+            val intent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+            startActivity(intent)
+        } else {
+            Toast.makeText(
+                this,
+                "Your location provider is already ON",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
     }
+
+
+    // A function to verify that the user location is turned on
+    private fun isLocationEnabled(): Boolean {
+        val locationManager: LocationManager =
+                getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) ||
+                locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
+    }
+
 }
